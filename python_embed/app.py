a031e55d03e2291602ac91549c2b62f9f224d412
Shivam23coder <shivam23100@gmail.com>
2025-07-01 14:59:49 +0530

    Added requirement and added default port

--- python_embed/app.py
+++ python_embed/app.py
@@ -13,5 +13,8 @@ def embed():
     embedding = [ord(c) % 10 / 10 for c in text][:10]
     return jsonify({'embedding': embedding})
 
+import os
+
 if __name__ == '__main__':
-    app.run(port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
