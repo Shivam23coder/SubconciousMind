2333b79ddb4171769eac0623c72a6e0d438bd86b
Shivam23coder <shivam23100@gmail.com>
2025-07-26 08:27:33 +0530

    checnged setting

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_BASE = 'https://subconciousmind.onrender.com/';
+const API_BASE = 'https://subconciousmind.onrender.com/apex/items';
 
+// export const getAll = (data) => axios.post(`${API_BASE}/`, data);
 export const addItem = (data) => axios.post(`${API_BASE}/add`, data);
-// export const getAllItems = () => axios.get(`${API_BASE}/`);
+export const getAllItems = () => axios.get(`${API_BASE}/`);
 export const searchItem = (query) => axios.post(`${API_BASE}/search`, { query });
 // export const searchByKeyword = (keyword) => axios.get(`${API_BASE}/searchByKeyword`, { params: { keyword } });
 // export const updateItem = (id, data) => axios.put(`${API_BASE}/update/${id}`, data);
