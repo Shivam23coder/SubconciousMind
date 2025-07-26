04b48ea383e25f01871010a3ff0ef28065a70e7e
Shivam23coder <shivam23100@gmail.com>
2025-07-26 12:26:06 +0530

    Tensorflow updated

--- backend/utils/embedder.js
+++ backend/utils/embedder.js
@@ -1,5 +1,6 @@
 import * as use from '@tensorflow-models/universal-sentence-encoder';
-import * as tf from '@tensorflow/tfjs-node';
+// import * as tf from '@tensorflow/tfjs-node';
+import * as tf from '@tensorflow/tfjs';
 
 let model = null;
 
